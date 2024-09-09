609c56d506b40e40886586f3fbd640f4876234c2
Admin <Admin@Admin>
2024-09-09 13:17:16 +0700

    demo

---
+++ src/main/java/demo/gui/laptrinhgui/LapTrinhGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package demo.gui.laptrinhgui;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class LapTrinhGui {
+
+    public static void main(String[] args) {
+        
+    }
+}
